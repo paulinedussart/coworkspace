3ab247a48dda94ff17a4f42b07ea637edc1ee9bf
Pauline Dussart <paulinedussart04@gmail.com>
2019-06-04 12:26:09 +0200

    install geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_140555) do
+ActiveRecord::Schema.define(version: 2019_06_04_095953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 2019_06_03_140555) do
     t.bigint "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["owner_id"], name: "index_spaces_on_owner_id"
   end
 

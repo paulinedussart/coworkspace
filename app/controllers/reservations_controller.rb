565a2d7a007eb50053f5b97725daff9402c91a35
Ludivine Robin <ludivine.robin@kedgebs.com>
2019-06-06 17:10:31 +0200

    begin reservation create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,12 +9,26 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = current_user.reservation.build(reservation_params)
+    @desk = S
     @reservation.desk = @desk
     @reservation.status = "Pending"
+
+    respond_to do |format|
+      if @reservation.save
+        format.html { redirect_to root_path, notice: 'Thank you for your booking ! Your reservation is pending. You will receive an email in the next 24h to confirm your reservation !' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
+  def status?
+    # "pending", "confirm" and "refused"
+  end
 
   private
 
-  params.require(:reservation).permit(:arrival_date, :departure_date, :status)
+  def reservation_params
+    params.require(:reservation).permit(:arrival_date, :departure_date, :status)
+  end
 end

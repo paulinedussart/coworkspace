ae514b861fd089685e79f1fd9da265169a7e8ce5
Aurelie GIMET <aurelie.gimet@kedgebs.com>
2019-06-06 18:05:56 +0200

    review australie crééees

--- db/seeds.rb
+++ db/seeds.rb
@@ -181,7 +181,41 @@ reservation2 = Reservation.create!(
   )
 puts ' --> RESERVATIONS created for Havana Beach'
 
-Review1 = review.create!(content: "From the day we moved in to Havana Beach, it has met all our needs professionally and quickly. The environment is perfect for a small and rapidly growing business, with so much space to accommodate small client meetings and larger marketing or networking events.", rating: "5", user: "aure", space: havana_beach, created_at: "04/06/19", updated_at: "18:00")
+puts ' --> creating REVIEWS for Havana Beach'
+review1 = review.create!(
+  content: "From the day we moved in to Havana Beach, it has met all our needs professionally and quickly. The environment is perfect for a small and rapidly growing business, with so much space to accommodate small client meetings and larger marketing or networking events.",
+  rating: "5",
+  user: "aure",
+  space: "havana_beach",
+  created_at: "04/06/19",
+  updated_at: "18:00")
+
+review2 = review.create!(
+  content: "Work Inc has the fastest internet we could find, mixed with the coolest warehouse vibe, strong community feel and reasonable pricing.",
+  rating: "4",
+  user: "lulu",
+  space: "havana_beach",
+  created_at: "03/06/19",
+  updated_at: "10:00")
+
+review3 = review.create!(
+  content: "Great working environment with a super dedicated team ensuring everything runs smoothly.",
+  rating: "5",
+  user: "pierre",
+  space: "havana_beach",
+  created_at: "05/06/19",
+  updated_at: "18:00")
+
+review4 = review.create!(
+  content: "Great location, creative design, vibrant community and friendly staff! I’ve been working at Work Inc for months and seeing how the team is making continuous improvement where things are already good enough. Thanks team! Highly recommended.",
+  rating: "5",
+  user: "marc",
+  space: "havana_beach",
+  created_at: "04/05/19",
+  updated_at: "18:00")
+puts ' --> REVIEWS created for Havana Beach'
+
+
 
 url = "https://coworker.imgix.net/photos/australia/sydney/wework-martin-place/2.jpg"
 we_work_martin_place = Space.new(name: "We Work Martin Place", address: "5 Martin Place, Sydney, Australia", description: "We Work is the worlds most unique coworking space, tucked within the foundations of the iconic Sydney Harbour Bridge at Lavender Bay. This heritage space has been reimagined to cultivate a community of driven start-ups and businesses created by entrepreneurs for entrepreneurs.
@@ -254,6 +288,43 @@ desk300 = Desk.create!(
   space: work_in
   )
 puts ' --> DESKS created for Work In'
+
+
+puts ' --> creating REVIEWS for Work In'
+review1 = review.create!(
+  content: "Fantastic location close to both North Sydney and Milsons Point stations. Great facilities and the staff are always on hand and very helpful. A very integrated community feel between companies. An all round great operation!",
+  rating: "5",
+  user: "aure",
+  space: "work_in",
+  created_at: "02/06/19",
+  updated_at: "18:00")
+
+review2 = review.create!(
+  content: "When I tell people I work in this space they get the serious case of the “jeals”. Everything you could possibly need in a workspace (and more) is catered for here.",
+  rating: "4",
+  user: "lulu",
+  space: "work_in",
+  created_at: "03/06/19",
+  updated_at: "10:00")
+
+review3 = review.create!(
+  content: "Well located site , right in the heart of Sydney, near the one of most popular train station in town. Comfortable offices across different industries, which make colleagues interesting people to talk.",
+  rating: "5",
+  user: "pierre",
+  space: "work_in",
+  created_at: "05/06/19",
+  updated_at: "18:00")
+
+review4 = review.create!(
+  content: "Work in has amazing facilities, it is a fantastic welcoming place to work. The location is great too.",
+  rating: "5",
+  user: "marc",
+  space: "work_in",
+  created_at: "04/05/19",
+  updated_at: "18:00")
+puts ' --> REVIEWS created for Work In'
+
+
 # SEEDS AUSTRALIA END
 
 

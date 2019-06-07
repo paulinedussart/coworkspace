7364bb828915e6c0c666ff66cde5cb1ff20aa5dc
Pauline Florens <pauflorens@gmail.com>
2019-06-07 16:58:54 +0200

    modifs v1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_105649) do
+ActiveRecord::Schema.define(version: 2019_06_07_145405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_105649) do
     t.string "name"
     t.string "siret"
     t.string "phone"
+    t.string "avatar"
     t.index ["email"], name: "index_owners_on_email", unique: true
     t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
   end
@@ -83,6 +84,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_105649) do
     t.bigint "space_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
     t.index ["space_id"], name: "index_reviews_on_space_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end

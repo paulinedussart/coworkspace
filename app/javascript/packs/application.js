1fb0e354a531469a73aa2b38f24fd5b7f254055e
Pauline Dussart <paulinedussart04@gmail.com>
2019-06-10 15:04:40 +0200

    photos fix ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,15 @@
 import "bootstrap";
 import activeCheckBoxes from "./service_choice";
-import bookReservation from "./book_reservation";
-
-activeCheckBoxes();
-bookReservation();
-
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
 
 initMapbox();
+import bookReservation from "./book_reservation";
+
+activeCheckBoxes();
+bookReservation();
+
 
 import "../plugins/flatpickr"

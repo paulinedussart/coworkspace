1bb9c514ef34e3620eccc1e59b54cc0c6db24fb5
Pauline Dussart <paulinedussart04@gmail.com>
2020-11-24 21:36:52 +0100

    fix disposition in show and error JS for navbar effect

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -1,11 +1,13 @@
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-window.onscroll = function() {scrollNavbarFunction()};
-
+const navbar = document.getElementById("theNavbar");
+if (navbar) {
+	// When the user scrolls down 20px from the top of the document, slide down the navbar
+	window.onscroll = function() {scrollNavbarFunction()};
+}
 function scrollNavbarFunction() {
   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-    document.getElementById("theNavbar").style.backgroundColor = "white";
-    document.getElementById("theNavbar").style.transition = "0.3s";
+    navbar.style.backgroundColor = "white";
+    navbar.style.transition = "0.3s";
   } else {
-    document.getElementById("theNavbar").style.backgroundColor = "transparent";
+    navbar.style.backgroundColor = "transparent";
   }
 }

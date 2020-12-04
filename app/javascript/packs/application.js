22b422678658160f8a8d19c3055996f85631f4e7
Pauline Dussart <paulinedussart04@gmail.com>
2020-12-04 08:47:57 +0100

    clean app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,18 +2,18 @@
 import "bootstrap";
 
 import activeCheckBoxes from "./service_choice";
+activeCheckBoxes();
+
+// ---------- MapBox ----------
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
-
-// ---------- MapBox ----------
 initMapbox();
 
-
+// ---------- resaCalculDuration ----------
 import bookReservation from "./book_reservation";
 bookReservation();
 
-activeCheckBoxes();
 // ---------- Algolia ----------
 import { initAutocomplete } from "../plugins/init_autocomplete";
 initAutocomplete();

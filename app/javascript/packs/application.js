c5539a77cf847fc6d7a7479963b0cc4648ae201b
Pauline Dussart <paulinedussart04@gmail.com>
2020-12-04 11:08:16 +0100

    install full callendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,12 @@ import "bootstrap";
 import activeCheckBoxes from "./service_choice";
 activeCheckBoxes();
 
+// ------------ Calendar ------------
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+import fullCalendar from "./fullcalendar";
+fullCalendar();
+
 // ---------- MapBox ----------
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';

6bc2587c64f4b8d54a91c41e9ec255e4cedca438
Pauline Dussart <paulinedussart04@gmail.com>
2020-12-04 10:05:47 +0100

    delete useless image and js code for message

--- app/assets/javascripts/scroll.js
+++
@@ -1,8 +0,0 @@
-function scrollLastMessageIntoView() {
-  var messages = document.querySelectorAll('.message');
-  var lastMessage = messages[messages.length - 1];
-
-  if (lastMessage !== undefined) {
-    lastMessage.scrollIntoView();
-  }
-}
